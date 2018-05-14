35443aebe854c0a3d884250e80be1e9d7fd0b249
Bin Zai <ybxxx070@126.com>
2018-05-14 16:26:10 +0800

    666

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
-db=os.environ('DB_LINK')
+db=os.environ.get('DB_LINK')
 
 
 ###
