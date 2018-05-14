f85720a7411c1fa2ee6668fdedfa7934e72615bf
boybing <ybxxx070@126.com>
2018-05-14 23:15:33 +0800

    Update app.py
    666

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def home():
     """Render website's home page."""
     #mongo.insert([{'xx':'xx'}])
     app.config['DB_LINK'] = os.environ.get('DB_LINK')
-    print app
+    print app.config
     return "Hello World !".encode()
 
 
