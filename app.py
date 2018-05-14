b7ac70a81bea23c4d832840cddd8b22cf7c0dcb1
Bin Zai <ybxxx070@126.com>
2018-05-14 16:24:35 +0800

    666

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
+db=os.environ('DB_LINK')
 
 
 ###
@@ -22,7 +23,7 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configur
 @app.route('/')
 def home():
     """Render website's home page."""
-    mongo.insert([{'xx':'xx'}])
+    mongo.insert(db,[{'xx':'xx'}])
     return "Hello World !".encode()
 
 
--- mongo.py
+++ mongo.py
@@ -1,21 +1,21 @@
 from  pymongo  import  MongoClient
 import os
 
-def insert(data):
-        client = MongoClient(os.environ.get['DB_LINK'])
+def insert(dblink,data):
+        client = MongoClient(dblink)
         db= client.bzbz.dazhong
         print(db.insert(data))
 
-def find():
-        client = MongoClient(os.environ.get['DB_LINK'])
+def find(dblink):
+        client = MongoClient(dblink)
         db= client.bzbz.dazhong
         for i in db.find():
             print(i)
 
-def remove():
+def remove(dblink):
         type=input("delet all?y or n")
         if type is 'y':
-                client = MongoClient(os.environ.get['DB_LINK'])
+                client = MongoClient(dblink)
                 db = client.bzbz.dazhong
                 db.remove()
         else:
