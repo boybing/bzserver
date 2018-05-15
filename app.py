182209cc3de88789a95e68a751e1f101dec4b5aa
boybing <ybxxx070@126.com>
2018-05-15 08:58:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configur
 @app.route('/')
 def home():
     """Render website's home page."""
-    db=app.config['DB_LINK']
+    #db=app.config['DB_LINK']
     #mongo.insert(db,[{'xx':'xx'}])
     #app.config['DB_LINK'] = os.environ.get('DB_LINK')
      #print app.config
