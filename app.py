e3f46429d16774048f86536395e95b555fd21453
Bin Zai <ybxxx070@126.com>
2018-09-19 16:24:13 +0800

    111

--- app.py
+++ app.py
@@ -6,14 +6,20 @@ Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
 
+import dazhongdianpin as dz
 import os
 import mongo
+import time
+import requests
+from bs4 import BeautifulSoup
+
 from flask import Flask, render_template, request, redirect, url_for
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
-app.config['DDDD']=os.environ['DDDD']
-app.config['DB_LINK']=os.environ['DB_LINK']
+# app.config['DDDD']=os.environ['DDDD']
+# app.config['DB_LINK']=os.environ['DB_LINK']
 
 ###
 # Routing for your application.
@@ -22,14 +28,59 @@ app.config['DB_LINK']=os.environ['DB_LINK']
 @app.route('/')
 def home():
     """Render website's home page."""
-    # mongo.insert([{'xx':'xx'}])
-    print app.config['DDDD']
-    #db=app.config['DB_LINK']
-    mongo.insert(app.config['DB_LINK'],[{'xx':'xx'}])
-    #app.config['DB_LINK'] = os.environ.get('DB_LINK')
-    print app.config['SECRET_KEY']
+    # mongo.insert(app.config['DB_LINK'],[{'xx':'xx'}])
 
     return "Hello World !".encode()
+
+@app.route('/d/<id>')
+def ariticle(id):
+    # dz.dblink=app.config['DB_LINK']
+    # dz.dazhongdianpin('',id)
+    # f = open(r'test.txt', 'r')  # 打开所保存的cookies内容文件
+    cookies = {}  # 初始化cookies字典变量
+    for line in '_lxsdk_cuid=162dd1afa327c-0adaa32fabe195-33697b04-fa000-162dd1afa33c8; _lxsdk=162dd1afa327c-0adaa32fabe195-33697b04-fa000-162dd1afa33c8; _hc.v=b6a8900b-a829-9d67-3821-69ad020e7290.1524127956; s_ViewType=10; ua=dpuser_8775393991; ctu=fd0193bb6215b37ebea4235d05b07e6d84c24619e199b41fa00b2b816b5cecdf; UM_distinctid=162dd2447d7480-0479774a4488e6-33697b04-fa000-162dd2447d8b53; aburl=1; QRCodeBottomSlide=hasShown; cy=3; cye=hangzhou; ctu=3219540c83f3acb32b2ffdf8de65df8dc294f04f715c1d427b2a30caac0a5a195cf74e7ebbda8e66ac1526b4b2812fb7; _lxsdk_s=16367f63de6-505-aa0-a5a%7C%7C38'.split(';'):  # 按照字符：进行划分读取
+        # 其设置为1就会把字符串拆分成2份
+        name, value = line.strip().split('=', 1)
+        cookies[name] = value  # 为字典cookies添加内容
+
+    session=requests.session()
+    headers = {'Accept':'image/webp,image/apng,image/*,*/*;q=0.8',
+'Accept-Encoding':'gzip, deflate',
+'Accept-Language':'zh-CN,zh;q=0.9',
+'Connection':'keep-alive',
+'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36'}
+    r = session.get(url= 'https://www.dianping.com',headers=headers)
+    resp=session.get(url='http://www.dianping.com/search/keyword/3/0_%E4%B8%87%E8%B1%A1%E5%9F%8E',headers=headers)
+    print(r.request.headers)
+    print(r.headers)
+    print(r.cookies)
+    print(r.status_code)
+    print(resp.request.headers)
+    print(resp.headers)
+    print(resp.cookies)
+    print(resp.status_code)
+    html=resp.text
+    print(html)
+
+    bs = BeautifulSoup(html, "html.parser")
+    dd = bs.find_all(name='div', attrs={"class": "txt"})
+    ee = bs.find_all(name='div', attrs={"class": "comment"})
+    ff = bs.find_all(name='span', attrs={"class": "addr"})
+    gg = bs.find_all(name='a', attrs={"class": "mean-price"})
+    vl=[]
+    for i in range(len(gg)):
+        try:
+            if gg[i].b is None:
+                vl.append({'title': dd[i].a.h4.string, 'price': "￥0", 'stars': ee[i].span.get('title'),
+                           'address': ff[i].string, "review": ee[i].a.b.string,"url":dd[i].a.get('href'),"time":time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))})
+            else:
+                vl.append({'title': dd[i].a.h4.string, 'price': gg[i].b.string, 'stars': ee[i].span.get('title'),
+                           'address': ff[i].string, "review": ee[i].a.b.string,"url":dd[i].a.get('href'),"time":time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))})
+        except:
+            vl.append({'title': dd[i].a.h4.string, 'price': "￥0", 'stars': ee[i].span.get('title'),
+                       'address': ff[i].string, "review": "0","url":dd[i].a.get('href'),"time":time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))})
+    print(vl)
+    return html
 
 
 @app.route('/about/')
---
+++ dazhongdianpin.py
@@ -0,0 +1,104 @@
+import time,random,ssl
+from urllib import request
+from urllib.parse import quote
+from bs4 import BeautifulSoup
+from openpyxl import Workbook
+import mongo as db
+
+vl = []
+def reduceSame(vl):
+    arr=[]
+    for id in vl:
+        if id not in arr:
+            arr.append(id)
+    # print(arr)
+    return arr
+
+def reqst(str, fg, count):
+    if fg==1:
+        req = request.Request(quote(str, safe='/:?='))
+    else:
+        req = request.Request(str)
+    req.add_header('User-Agent',
+                   'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6')
+    with request.urlopen(req) as response:
+        html = response.read().decode("utf-8")
+
+    bs = BeautifulSoup(html, "html.parser")
+    dd = bs.find_all(name='div',attrs={"class":"txt"})
+    ee = bs.find_all(name='div', attrs={"class": "comment"})
+    ff = bs.find_all(name='span',attrs={"class":"addr"})
+    gg = bs.find_all(name='a',attrs={"class":"mean-price"})
+
+    for i in range(len(gg)):
+        try:
+            if gg[i].b is None:
+                vl.append({'title': dd[i].a.h4.string, 'price': "￥0", 'stars': ee[i].span.get('title'),
+                           'address': ff[i].string, "review": ee[i].a.b.string,"url":dd[i].a.get('href'),"time":time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))})
+            else:
+                vl.append({'title': dd[i].a.h4.string, 'price': gg[i].b.string, 'stars': ee[i].span.get('title'),
+                           'address': ff[i].string, "review": ee[i].a.b.string,"url":dd[i].a.get('href'),"time":time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))})
+        except:
+            vl.append({'title': dd[i].a.h4.string, 'price': "￥0", 'stars': ee[i].span.get('title'),
+                       'address': ff[i].string, "review": "0","url":dd[i].a.get('href'),"time":time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))})
+    c=bs.find(name='a', attrs={"class": "next"})
+
+    try:
+        time.sleep(random.randint(0, 1000) * 0.001)
+        print("已完成第%i页数据添加"%count)
+        count=count+1
+        reqst(c.get('href'),0,count)
+    except AttributeError as e:
+        # print(reduceSame(vl))
+        # db.insert(vl)
+        db.insert(vl)
+        db.find()
+        # wb = Workbook()
+        # ws = []
+        # ws.append(wb.create_sheet(title='单页搜索结果'))
+        # ws[0].append(['商家', '人均单价', '星级', '地址', '点评数',"数据更新时间"])
+        # for i in reduceSame(vl):
+        #     ws[0].append(['=HYPERLINK("' + i['url'] + '","' + i['title'] + '")',i['price'],i['stars'],i['address'],i['review'],i['time']])
+        # save_path = "搜索结果.xlsx"
+        # wb.save(save_path)
+
+
+# print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+
+
+class dazhongdianpin:
+    def __init__(self, dblink, id):
+        self.dblink = dblink
+        self.id = id
+        self.start()
+
+    def start(self):
+        ssl._create_default_https_context = ssl._create_unverified_context
+        str = "http://www.dianping.com/search/keyword/3/0_" + self.id
+        reqst(str, 1, 1)
+# 附带不转换字符参数
+# print('\n附加不转换字符参数：\n%s' % quote(str, safe='/:?='))
+    # str = re.sub(r'%25+', '%', str)
+
+# from urllib import request
+#
+# with request.urlopen('https://api.douban.com/v2/book/2129650') as f:
+#     data = f.read()
+#     print('Status:', f.status, f.reason)
+#     for k, v in f.getheaders():
+#         print('%s: %s' % (k, v))
+#     print('Data:', data.decode('utf-8'))
+
+# wd=wordDeliver.deliver("线程是程序执行时的最小单位，它是进程的一个执行流，\
+# #         是CPU调度和分派的基本单位，一个进程可以由很多个线程组成，\
+# #         线程间共享进程的所有资源，每个线程有自己的堆栈和局部变量。\
+# #         线程由CPU独立调度执行，在多CPU环境下就允许多个线程同时运行。\
+# #         同样多线程也可以实现并发操作，每个请求分配一个线程来处理。")
+# print(wd.data)
+
+
+# t1=['a','b']
+# t2=['c']
+# t1.append(t2)
+# print(t1)
+# print('c' in t1)
